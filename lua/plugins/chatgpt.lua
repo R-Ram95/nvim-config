147d8802f82fb5771c8f115d0e7304f81b250d1d
R-Ram95 <rohinesh@ualberta.ca>
2024-10-18 12:52:38 -0600

    add chatgpt and obsidian plugins

---
+++ lua/plugins/chatgpt.lua
@@ -0,0 +1,32 @@
+return {
+	"jackMort/ChatGPT.nvim",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"nvim-lua/plenary.nvim",
+		"folke/trouble.nvim", -- optional
+		"nvim-telescope/telescope.nvim",
+	},
+	event = "VeryLazy",
+	config = function()
+		local home = vim.fn.expand("$HOME/Documents/keys")
+		local chatgpt = require("chatgpt")
+
+		require("chatgpt").setup({
+			api_key_cmd = "gpg --decrypt " .. home .. "/openai.txt.gpg",
+			openai_params = {
+				model = "gpt-3.5-turbo",
+				frequency_penalty = 0,
+				presence_penalty = 0,
+				max_tokens = 4095,
+				temperature = 0.2,
+				top_p = 0.1,
+				n = 1,
+			},
+		})
+
+		-- Bind the key to ChatGPT functionality without using which-key
+		vim.keymap.set("n", "<leader>c", function()
+			chatgpt.edit_with_instructions()
+		end, { desc = "Edit with instructions" })
+	end,
+}
