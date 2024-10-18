9601f9c80b4e7b90116a06cad60530530ea1def9
R-Ram95 <rohinesh@ualberta.ca>
2024-10-17 21:45:14 -0600

    remove extra configs from theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -11,10 +11,6 @@ return {
 				transparent = true,
 			})
 			vim.cmd("colorscheme dracula")
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
 		end,
 	},
 }
