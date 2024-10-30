1eeb1b1284cde5edc2035e3bf2f74d104a574203
R-Ram95 <rohinesh@ualberta.ca>
2024-10-30 13:02:35 -0600

    update whichkey, obsidian, chatgpt
    - added opening external links in obsidian
    - added key binds for chatgpt

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -21,32 +21,6 @@ return {
 				top_p = 0.1,
 				n = 1,
 			},
-			-- Automatically open help, settings, and session panels on start
-			chat = {
-				keymaps = {
-					-- Custom keymaps to toggle panels after opening ChatGPT
-					toggle_sessions = "<C-p>", -- Keybinding to open sessions panel
-					toggle_help = "<C-h>", -- Keybinding to open help panel
-					toggle_settings = "<C-o>", -- Keybinding to open settings panel
-				},
-			},
 		})
-
-		-- Custom function to open ChatGPT and trigger the panels
-		local function open_chatgpt_with_panels()
-			vim.cmd("ChatGPT") -- Open ChatGPT chat
-			-- Automatically trigger the keybindings to open panels
-			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-o>", true, false, true), "n", false) -- Open settings
-			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-h>", true, false, true), "n", false) -- Open help
-			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, false, true), "n", false) -- Open sessions
-		end
-
-		-- Keymap
-		vim.keymap.set(
-			"n",
-			"<leader>c",
-			open_chatgpt_with_panels,
-			{ desc = "Open ChatGPT with settings, help, and sessions" }
-		)
 	end,
 }
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -15,4 +15,31 @@ return {
 			desc = "Buffer Local Keymaps (which-key)",
 		},
 	},
+	config = function()
+		local wk = require("which-key")
+
+		wk.add({
+			{ "<leader>c", group = "ChatGPT" },
+			{ "<leader>cc", "<cmd>ChatGPT<CR>", desc = "ChatGPT" },
+			{
+				mode = { "n", "v" },
+				{ "<leader>ca", "<cmd>ChatGPTRun add_tests<CR>", desc = "Add Tests" },
+				{ "<leader>cd", "<cmd>ChatGPTRun docstring<CR>", desc = "Docstring" },
+				{ "<leader>ce", "<cmd>ChatGPTEditWithInstruction<CR>", desc = "Edit with instruction" },
+				{ "<leader>cf", "<cmd>ChatGPTRun fix_bugs<CR>", desc = "Fix Bugs" },
+				{ "<leader>cg", "<cmd>ChatGPTRun grammar_correction<CR>", desc = "Grammar Correction" },
+				{ "<leader>ck", "<cmd>ChatGPTRun keywords<CR>", desc = "Keywords" },
+				{
+					"<leader>cl",
+					"<cmd>ChatGPTRun code_readability_analysis<CR>",
+					desc = "Code Readability Analysis",
+				},
+				{ "<leader>co", "<cmd>ChatGPTRun optimize_code<CR>", desc = "Optimize Code" },
+				{ "<leader>cr", "<cmd>ChatGPTRun roxygen_edit<CR>", desc = "Roxygen Edit" },
+				{ "<leader>cs", "<cmd>ChatGPTRun summarize<CR>", desc = "Summarize" },
+				{ "<leader>ct", "<cmd>ChatGPTRun translate<CR>", desc = "Translate" },
+				{ "<leader>cx", "<cmd>ChatGPTRun explain_code<CR>", desc = "Explain Code" },
+			},
+		})
+	end,
 }
